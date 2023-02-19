bc5dd6609eef9c94766f3e748269a0f3025373f2
vvim96 <galin.angelow@gmail.com>
2023-02-19 18:56:59 +0200

    Substituted some variables

--- app/auditd_logwatcher-py3.py
+++ app/auditd_logwatcher-py3.py
@@ -3,13 +3,12 @@ import time
 log_file = "/var/log/audit/audit-raw.log"
 string1_to_watch = "tty=(none)"
 string2_to_watch = "res=1"
-replacement_string = ""
 
 with open (log_file, "r") as file, open("/var/log/audit/audit.log", "w") as file_modified:
     while True:
         for line in file:
             if string1_to_watch in line and string2_to_watch in line:
-                line = line.replace(string2_to_watch, replacement_string)
+                line = line.replace(string2_to_watch, '')
             file_modified.write(line)
         file_modified.flush()
         time.sleep(1)
